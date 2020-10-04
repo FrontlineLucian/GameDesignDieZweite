38413894730170e658595732c666e877529c4477
8BitKing <64686498+8BitKing@users.noreply.github.com>
2020-10-04 19:32:57 +0200

    elevator

---
+++ Gamedesign2020/Assets/Scripts/ElevatorOpen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorOpen : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "KIND")
+        {
+            if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("ElevatorClosed")){
+                this.animator.Play("Elevator", -1, 0);
+            }
+        }
+
+    }
+}
