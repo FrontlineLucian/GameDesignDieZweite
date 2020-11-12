e39b33920d4f50fd535c9474c663064ad9a9ca6f
8BitKing <64686498+8BitKing@users.noreply.github.com>
2020-11-12 09:58:19 +0100

    menu start

---
+++ Gamedesign2020/Assets/ButtonsFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsFunctions : MonoBehaviour
+{
+    public void __SelectLevel()
+    {
+        SceneManager.LoadScene("__MENU_SELECT_LEVEL", LoadSceneMode.Single);
+    }
+    public void __Continue()
+    {
+        SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Single);
+    }
+    public void __Exit()
+    {
+        
+    }
+
+    public void __Back()
+    {
+        SceneManager.LoadScene("__MENU", LoadSceneMode.Single);
+    }
+}
